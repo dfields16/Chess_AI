c686980fa4982a21528e50b25c27b6940dc50acb
Dawson <dfields16@tamu.edu>
2019-03-21 10:45:31 -0500

    Finish Proj Setup

--- Project/src/Driver.java
+++ Project/src/Driver.java
@@ -2,7 +2,19 @@
 public class Driver {
 
 	public static void main(String[] args) {
-		System.out.println("Howdy");
+		String wPawn = "♙";
+		String wRook = "♖";
+		String wKnight = "♘";
+		String wBishop = "♗";
+		String wQueen = "♕";
+		String wKing = "♔";
+		String bPawn = "♟";
+		String bRook = "♜";
+		String bKnight = "♞";
+		String bBishop = "♝";
+		String bQueen = "♛";
+		String bKing = "♚";
+		
 
 	}
 
