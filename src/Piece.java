f6c9300aaa216967741bb96cec48680a54cea6a4
tateleed <tateleed@tamu.edu>
2019-04-04 10:10:42 -0500

    basic piece values

--- src/Piece.java
+++ src/Piece.java
@@ -1,4 +1,18 @@
-enum ChessPiece {EMPTY, KING, QUEEN, BISCHOP, KNIGHT, ROOK, PAWN}
+enum ChessPiece {
+	EMPTY 	(0), 
+	KING 	(100), 
+	QUEEN 	(75), 
+	BISCHOP (25), 
+	KNIGHT	(50), 
+	ROOK 	(25), 
+	PAWN 	(10);
+	
+
+	final int value;
+	
+	ChessPiece (int v){value = v;}
+
+}
 
 class Piece {
 
@@ -10,6 +24,7 @@ class Piece {
   int checked;
 
   public Piece(ChessPiece p, int c) {
+//	int i = ChessPiece.BISCHOP.value;
     type = p;
     side = c;
   }
