2975519fd91e3f9dc990a88e395825bf0800d8ef
Dawson <dfields16@tamu.edu>
2019-04-14 19:40:40 -0500

    Removed Files

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import java.io.IOException;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 
 public class Main{
 
   public static void main(String[] args) throws IOException
   {
-
+    setLookAndFeel();
     JFrame frame = new JFrame("Chess Game");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(1020, 765);
@@ -17,4 +19,15 @@ public class Main{
     frame.setVisible(true);
   }
 
+  static void setLookAndFeel(){
+    try {
+      for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+          if ("Nimbus".equals(info.getName())) {
+              UIManager.setLookAndFeel(info.getClassName());
+              break;
+          }
+      }
+  } catch (Exception e) {
+  }
+  }
 }
